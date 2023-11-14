f37f76376c76dee9d62c0311b6ed3ebefc4fc833
AcacioTelechi <acaciotelechi@gmail.com>
2023-11-14 17:13:56 -0300

    feat: create generate_date and create_folder_if_not_exists funcs

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from time import sleep
 import xmltodict
@@ -45,3 +46,58 @@ def request(url) -> list[dict]:
         else:
             raise ConnectionError(response.status_code)
     return arqv
+
+
+def create_folder_if_not_exists(folder_path):
+    """
+    Check if a folder exists, and create it if it doesn't.
+
+    Parameters:
+    - folder_path (str): Path of the folder to check and create.
+
+    Returns:
+    - None
+    """
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+
+
+# Example usage:
+folder_path = "path/to/your/folder"
+
+create_folder_if_not_exists(folder_path)
+
+
+from datetime import datetime, timedelta
+
+
+def generate_dates(start_date, end_date, step=1, **kwargs):
+    """
+    Generate dates between a start date and an end date with a specified step size.
+
+    Parameters:
+    - start_date (str or datetime): Start date in 'YYYY-MM-DD' format or as a datetime object.
+    - end_date (str or datetime): End date in 'YYYY-MM-DD' format or as a datetime object.
+    - step (int): Number of days between consecutive dates (default is 1).
+
+    Returns:
+    - list of datetime objects representing the generated dates.
+    """
+    input_format = kwargs.get("input_format", "%Y-%m-%d")
+    output_format = kwargs.get("output_format", "%Y-%m-%d")
+    if isinstance(start_date, str):
+        start_date = datetime.strptime(start_date, input_format)
+    if isinstance(end_date, str):
+        end_date = datetime.strptime(end_date, input_format)
+
+    current_date = start_date
+    dates = []
+
+    while current_date <= end_date:
+        dates.append(current_date.strftime(output_format))
+        current_date += timedelta(days=step)
+
+    return dates
